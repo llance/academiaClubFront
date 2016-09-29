a4a32ebfd691f0ef07473bd248fb4b55225be889
Shuixi Li <shuixili@gmail.com>
2016-09-29 17:05:54 -0400

    main page looks close enough to requirement

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css',
+  '../assets/css/swiper.css']
 })
 export class BodyComponent implements OnInit {
 
