1eeac2cb5e5f713906aeca091fa2da875041b9a7
Shuixi Li <shuixili@gmail.com>
2016-10-10 20:21:45 -0400

    single import location for css but need to fix positioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    encapsulation: ViewEncapsulation.Native,
+    encapsulation: ViewEncapsulation.None,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css',
-    './assets/css/bootstrap.css',
-    './assets/css/style.css',
-    './assets/css/colors.css',
-    './assets/css/dark.css',
-    './assets/css/font-icons.css',
-    './assets/css/animate.css',
-    './assets/css/magnific-popup.css',
-    './assets/css/responsive.css',
-    './assets/css/medicalanimation.css',
-    './assets/css/google-css.css'
+    // './assets/css/bootstrap.css',
+    // './assets/css/style.css',
+    // './assets/css/colors.css',
+    // './assets/css/dark.css',
+    // './assets/css/font-icons.css',
+    // './assets/css/animate.css',
+    // './assets/css/magnific-popup.css',
+    // './assets/css/responsive.css',
+    // './assets/css/medicalanimation.css',
+    // './assets/css/google-css.css',
    ]
 })
 export class AppComponent {
--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -3,8 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css',
-  ]
+  // styles: [
+  //   require('../assets/css/google-css.css'),
+  //   require('../assets/css/bootstrap.css'),
+  //   require('../assets/css/style.css'),
+  //   require('../assets/css/colors.css'),
+  //   require('../assets/css/dark.css'),
+  //   require('../assets/css/font-icons.css'),
+  //   require( '../assets/css/animate.css'),
+  //   require('../assets/css/magnific-popup.css'),
+  //   require('../assets/css/responsive.css'),
+  //   require( '../assets/css/medicalanimation.css'),
+  //   // require('./assets/js/jquery.js'),
+  //   // require('./assets/js/plugins.js'),
+  //   // require('./assets/js/functions.js') 
+  //   ],
+  // styleUrls: ['./body.component.css',
+  // // '../assets/css/style.css',
+  // ]
 })
 export class BodyComponent implements OnInit {
 
