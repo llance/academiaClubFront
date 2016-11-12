f3cd986e9a56345c787e0c22985029f02bad30f2
Shuixi Li <shuixili@gmail.com>
2016-11-11 21:24:28 -0500

    code cleanup

--- src/app/conference/conference.component.ts
+++ src/app/conference/conference.component.ts
@@ -11,7 +11,7 @@ import { ConferenceApiService } from '../conference-api.service';
 })
 export class ConferenceComponent implements OnInit, AfterViewInit {
   private Conferences;
-  selectedEvent;
+  private selectedEvent;
   private specialityFilter;
   GeoFilter;
 
@@ -29,27 +29,23 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
   constructor(private _conferenceApiService: ConferenceApiService) {
   }
 
-  ngOnInit() {
-    console.log("called!");
-    // (<any>$('.selectpicker')).selectpicker('render');
-
+  ngOnInit() {    
     this.queryFilters();
-
-    this.sleep(500).then(() => {
-      (<any>$('.selectpicker')).selectpicker('render');
-    })
 
     this._conferenceApiService.getConference()
     .subscribe(
       Conferences => this.Conferences = Conferences,
       error => console.log('Error fetching conferences'));
+
+    this.sleep(1000).then(() => {
+      (<any>$('.selectpicker')).selectpicker('render');
+    })
   }
 
 
   ngAfterViewInit() {
-
     var __this = this._this;
-    // console.log("(<any>$('.selectpicker')) after view init is : ", (<any>$('.selectpicker')));
+
     (<any>$("#e1")).daterangepicker({
       applyOnMenuSelect: false,
       datepickerOptions: {
@@ -77,8 +73,6 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
     var startdate = seletectedRange.substring(10, 20);
     var enddate = seletectedRange.substring(seletectedRange.indexOf("end")+6, seletectedRange.indexOf("end")+16);
 
-    // console.log("selected end date : ", enddate);
-
     this._conferenceApiService.filterByDate(startdate, enddate)
     .subscribe(
       Conferences => this.Conferences = Conferences,
@@ -90,9 +84,6 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
     .subscribe(
       specialityFilter => this.specialityFilter = specialityFilter,
       error => console.log('Error fetching Specialites Filter'));
-
-    //this.options[0].value = this.specialityFilter.results[0].specialty_text;
-    // console.log("this.specialityFilter.results[0].specialty_text", this.specialityFilter.results[0].specialty_text);
 
     this._conferenceApiService.getGeoFilter()
     .subscribe(
@@ -107,9 +98,6 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
   }
 
   filterBySpeciality(event:string) {
-    // console.log("event is :", event);
-    // console.log("fetching events matching specialty filter");
-
     var startdate, enddate;
 
     var seletectedRange = JSON.stringify((<any>$("#e1")).daterangepicker("getRange"));
@@ -133,8 +121,6 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
     
   }
 
-
-
   filterByContinent(event: number) {
     // console.log("event is :", event);
     this.continentIndex = event;
@@ -143,7 +129,6 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
     console.log("GeoFilter.results[continentIndex] is :", this.GeoFilter.results[event].continent);
     (<any>$('.countrypicker')).selectpicker('render');
   }
-
 
   filterByCountry(event:number){
     (<any>$('.selectpicker')).selectpicker('render');
@@ -177,32 +162,5 @@ export class ConferenceComponent implements OnInit, AfterViewInit {
         error => console.log('Error fetching conferences'));
     }
   }
-
-
-
-  sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
-
-
-
-  loadjscssfile(filename : string, filetype: string){
-    console.log("loading file");
-    if (filetype=="js"){ //if filename is a external JavaScript file
-      var fileref_js=document.createElement('script')
-      fileref_js.setAttribute("type","text/javascript")
-      fileref_js.setAttribute("src", filename)
-      document.getElementsByTagName("head")[0].appendChild(fileref_js)
-    }
-    else if (filetype=="css"){ //if filename is an external CSS file
-      var fileref_css=document.createElement("link")
-      fileref_css.setAttribute("rel", "stylesheet")
-      fileref_css.setAttribute("type", "text/css")
-      fileref_css.setAttribute("href", filename)
-      document.getElementsByTagName("head")[0].appendChild(fileref_css)
-    }
-  }
-
-
 
 }
